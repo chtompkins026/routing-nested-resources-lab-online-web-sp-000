5aeb9e032dd28005747e287136a254ae8432ed01
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-18 15:44:53 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      
+      @songs = Aritst.find(params[:artist_id]).songs 
     else 
       @songs = Song.all
     end 
